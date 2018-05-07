748469d458b1df8318f13c8c1fcbe67fe808b075
Bruce Becker <brucellino@gmail.com>
2018-05-07 11:10:01 +0200

    Add dependencies to meta

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,19 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_slapd_process(host):
+    slapd_port = "tcp://0.0.0.0:2170"
+    assert host.socket(slapd_port).listening
