f616c2bb0c22073eae606b2805927d664efd7540
Bruce Becker <brucellino@gmail.com>
2018-05-08 08:16:25 +0200

    Add test and tasks for the bdii user

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+# The process should be run by the openldap user
+def test_slapd_user(host):
+    user = host.user('openldap')
+
+    assert user.exists
+    assert user.shell == '/usr/sbin/nologin'
+
+
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
 
@@ -14,11 +22,11 @@ def test_hosts_file(host):
     assert f.group == 'root'
 
 
-def test_slapd_process(host):
-    slapd_process = host.process.filter(comm="slapd")
-    assert slapd_process.running
-    slapd_port = "tcp://0.0.0.0:2170"
-    assert host.socket(slapd_port).is_listening
+# def test_slapd_process(host):
+#     slapd_process = host.process.filter(comm="slapd")
+#     assert slapd_process.running
+#     slapd_port = "tcp://0.0.0.0:2170"
+#     assert host.socket(slapd_port).is_listening
 
 
 def test_config_files(host):
@@ -35,9 +43,24 @@ def test_config_files(host):
 
 
 def test_log_files(host):
+    bdii_log_dir = host.file('/var/log/bdii')
     bdii_log_file = host.file('/var/log/bdii/bdii-update.log')
 
+    assert bdii_log_dir.exists
+    assert bdii_log_dir.is_dir
+    assert bdii_log_dir.owner == 'openldap'
+
     assert bdii_log_file.exists
+    assert bdii_log_file.is_file
+    assert bdii_log_file.owner
+
+
+def test_data_files(host):
+    data_dir = host.file('/var/lib/bdii')
+
+    assert data_dir.exists
+    assert data_dir.is_directory
+    assert data_dir.user == 'openldap'
 
 
 def test_cron_jobs(host):
