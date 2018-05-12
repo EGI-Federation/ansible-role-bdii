3a355844dd1a427d2c6d39812b34e983865320d8
Bruce Becker <brucellino@gmail.com>
2018-05-12 15:15:46 +0200

    Add matrix testing for different scenarios

--- molecule/site-bdii/tests/test_default.py
+++ molecule/site-bdii/tests/test_default.py
@@ -1,9 +1,19 @@
 import os
-
+# import requests
+# import xml.etree.ElementTree
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+# site = requests.get('https://goc.egi.eu/gocdbpi/public/
+# ?method=get_site&sitename=ZA-MERAKA')
+
+
+def test_slapd_user(host):
+    user = host.user('ldap')
+
+    assert user.exists
+    assert user.shell == '/usr/sbin/nologin'
 
 
 def test_hosts_file(host):
@@ -12,3 +22,33 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_config_files(host):
+    bdii_conf = host.file('/etc/bdii/bdii.conf')
+    bdii_sysconfig = host.file('/etc/sysconfig/bdii')
+
+    assert bdii_conf.exists
+    assert bdii_conf.is_file
+    assert bdii_conf.user == 'root'
+
+    assert bdii_sysconfig.exists
+    assert bdii_sysconfig.is_file
+
+    assert bdii_sysconfig.contains('^SLAPD_CONF\ = .*')
+
+
+def test_log_files(host):
+    bdii_log_dir = host.file('/var/log/bdii')
+
+    assert bdii_log_dir.exists
+    assert bdii_log_dir.is_directory
+    assert bdii_log_dir.user == 'ldap'
+
+
+def test_data_files(host):
+    data_dir = host.file('/var/lib/bdii')
+
+    assert data_dir.exists
+    assert data_dir.is_directory
+    assert data_dir.user == 'ldap'
